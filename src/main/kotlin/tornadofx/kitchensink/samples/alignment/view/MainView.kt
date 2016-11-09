09e2fbe8c8098f789130af838ee9ec84c4a55f43
Edvin Syse <es@syse.no>
2016-11-09 13:01:23 +0100

    Added all pos values to alignment sample

--- src/main/kotlin/tornadofx/kitchensink/samples/alignment/view/MainView.kt
+++ src/main/kotlin/tornadofx/kitchensink/samples/alignment/view/MainView.kt
@@ -10,138 +10,28 @@ import javafx.scene.paint.Color
 import tornadofx.*
 
 /**
- * A sample demonstrating the use of vboxConstraints and alignment
+ * A sample demonstrating the use of vbox constraints and alignment
  *
  */
 class MainView : View("Alignment") {
-	
-    override val root =
-			anchorpane {
+    override val root = anchorpane {
+        tilepane {
+            prefRows = 3
+            prefColumns = 3
+            padding = Insets(40.0)
+            hgap = 20.0
+            vgap = 20.0
 
-				tilepane {
-
-					prefRows = 3
-					prefColumns = 3
-					padding = Insets(40.0)
-					hgap = 20.0
-					vgap = 20.0
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("TOP_LEFT")
-						vboxConstraints {
-							alignment = Pos.TOP_LEFT
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("TOP_CENTER")
-						vboxConstraints {
-							alignment = Pos.TOP_CENTER
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("TOP_RIGHT")
-						vboxConstraints {
-							alignment = Pos.TOP_RIGHT
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("CENTER_LEFT")
-						vboxConstraints {
-							alignment = Pos.CENTER_LEFT
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("CENTER")
-						vboxConstraints {
-							alignment = Pos.CENTER
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("CENTER_RIGHT")
-						vboxConstraints {
-							alignment = Pos.CENTER_RIGHT
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("BOTTOM_LEFT")
-						vboxConstraints {
-							alignment = Pos.BOTTOM_LEFT
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("BOTTOM_CENTER")
-						vboxConstraints {
-							alignment = Pos.BOTTOM_CENTER
-						}
-					}
-
-					vbox {
-						background = Background(
-								BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0))
-						)
-						effect = DropShadow()
-						prefWidth = 150.0
-						prefHeight = 150.0
-						label("BOTTOM_RIGHT")
-						vboxConstraints() {
-							alignment = Pos.BOTTOM_RIGHT
-						}
-					}
-				}
-			}
+            Pos.values().forEach {
+                vbox {
+                    label(it.name)
+                    alignment = it
+                    background = Background(BackgroundFill(Color.TAN, CornerRadii(0.0), Insets(0.0)))
+                    effect = DropShadow()
+                    prefWidth = 150.0
+                    prefHeight = 150.0
+                }
+            }
+        }
+    }
 }
